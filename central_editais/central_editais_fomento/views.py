5064371b51e47d9043bc301761e4999e91b43022
Vinícius Alves Hax <viniciushax@gmail.com>
2019-12-12 10:54:53 -0300

    Adicionando view do index dos editais

--- central_editais/central_editais_fomento/views.py
+++ central_editais/central_editais_fomento/views.py
@@ -1,30 +1,32 @@
 # -*- coding: utf-8 -*
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from models import Editais
+from django.shortcuts import get_object_or_404
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the central editais fomento index.")
+	editais = Editais.objects.all()
+	context = {'editais': editais}
+	return render(request, 'index.html', context)
 
 # Comentei essa parte pois estava dando erro
-# from django.shortcuts import render
-# from django.http import HttpResponse
-# from djtools.utils import rtr, httprr
-# from django.contrib.auth.decorators import login_required
-# from models import Editais
-# from django.shortcuts import get_object_or_404
-# from datetime import datetime
 
 # @rtr()
-# def index(request):
-# 	editais = Editais.objects.all()
+#def index(request):
+#	editais = Editais.objects.all()
 # 	return locals()
 # #		return HttpResponse(u'Olá mundo!')
 
 # @rtr()
-# def busca(request):
-# 	return locals()
+#def busca(request):
+#	return locals()
 
 
 # @rtr()
-# def edital(request, edital_pk):
-# 	edital = get_object_or_404(Editais, id=edital_pk)
-# 	return locals()
+#def edital(request, edital_pk):
+#	edital = get_object_or_404(Editais, id=edital_pk)
+#	context = {'edital': edital}
+#	return render(request, 'edital.html', context)
+#	return locals()
